4d09e9251f6c6412f6be4812fcaf60013435ccc7
jose_maria_gonzalez_lopez <josegonzalezlopez@gmail.com>
2019-11-03 23:25:14 -0300

    Se agrega funcionalidades de modulos y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,11 @@
 import { RouterModule, Routes} from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficos1Component } from './pages/graficos1/graficos1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { RegisterComponent } from './login/register.component';
 
 
 const appRoutes: Routes = [
-    { path: '', component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficos1', component: Graficos1Component},
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-        ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent},
     { path: '**', component: NopagefoundComponent}
